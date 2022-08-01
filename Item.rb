0aa1073fed82d5dd082b9018467eb6af333b18e4
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-01 18:44:45 +0800

    Add all methods visible in the diagram

--- Item.rb
+++ Item.rb
@@ -7,4 +7,12 @@ class Item
     @publish_date = publish_date
     @archived = archived
   end
+
+  def can_be_archived?
+    return true if publish_date > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
 end
