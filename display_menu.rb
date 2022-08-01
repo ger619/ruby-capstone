6df22e79e48fe9d60cc60385af7fd5174d4fae98
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-01 20:55:10 +0800

    Separate display menus for each item

--- display_menu.rb
+++ display_menu.rb
@@ -1,14 +1,93 @@
+require_relative './exit_app'
+
 def display_menu
-  puts "\nWelcome to our Library!"
+  puts "\nWelcome to our Catalog!"
   puts "\nPlease choose an option by entering a number:"
   options = [
-    '1 - List all books, music albums, and games',
-    '2 - List all movies',
-    '3 - List all genres',
-    '4 - List all Labels',
-    '5 - List all Authors and Sources',
-    '6 - Add a book  or Add a music album or Add a game',
-    '7 - Exit'
+    '1 - For Books',
+    '2 - For Music Albums',
+    '3 - For Games',
+    '4 - Exit'
   ]
   puts options
+  choice = gets.chomp
+  case choice
+  when '1'
+    display_books
+  when '2'
+    display_music_albums
+  when '3'
+    display_games
+  when '4'
+    exit_app
+  end
+end
+
+def display_books
+  puts 'Enter selected tasks:'
+  options = [
+    '1 - List all books',
+    '2 - Add a book',
+    '3 - Back to menu'
+  ]
+  puts options
+  choice = gets.chomp
+  case choice
+  when '1'
+    list_books
+  when '2'
+    add_book
+  when '3'
+    display_menu
+  when '4'
+    exit_app
+  else
+    puts 'Invalid option'
+  end
+end
+
+def display_music_albums
+  puts 'Enter selected tasks:'
+  options = [
+    '1 - List all music albums',
+    '2 - Add a music album',
+    '3 - Back to menu'
+  ]
+  puts options
+  choice = gets.chomp
+  case choice
+  when '1'
+    list_music_albums
+  when '2'
+    add_music_album
+  when '3'
+    display_menu
+  when '4'
+    exit_app
+  else
+    puts 'Invalid option'
+  end
+end
+
+def display_games
+  puts 'Enter selected tasks:'
+  options = [
+    '1 - List all games',
+    '2 - Add a game',
+    '3 - Back to menu'
+  ]
+  puts options
+  choice = gets.chomp
+  case choice
+  when '1'
+    list_games
+  when '2'
+    add_game
+  when '3'
+    display_menu
+  when '4'
+    exit_app
+  else
+    puts 'Invalid option'
+  end
 end
--- main.rb
+++ main.rb
@@ -1,13 +1,11 @@
-# require_relative './app'
+require_relative './app'
 require_relative './display_menu'
-require_relative './choose_option'
 
 def main
   # app = App.new
-  # loop do
-  display_menu
-  choose_option
-  # end
+  loop do
+    display_menu
+  end
 end
 
 main
