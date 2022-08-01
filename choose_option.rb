236569df4fc6f62d714e99ca08caec9bfa6146f2
David Abol Ger <draker74@gmail.com>
2022-08-01 14:34:27 +0300

    Linters and cleanup

--- choose_option.rb
+++ choose_option.rb
@@ -4,25 +4,17 @@ def choose_option
   choice = gets.chomp
   case choice
   when '1'
-    app.display_books
+    app.display_books_music_albums_games
   when '2'
-    app.display_music_albums
-  when '3'
     app.display_movies
-  when '4'
+  when '3'
     app.display_genres
   when '5'
     app.display_labels
   when '6'
-    app.display_authors
+    app.display_authors_sources
   when '7'
-    app.display_sources
-  when '8'
-    app.add_book
-  when '9'
-    app.add_music_album
-  when '10'
-    app.add_game
+    app.add_book_or_music_album_or_game
   else
     exit_app
   end
--- display_menu.rb
+++ display_menu.rb
@@ -2,17 +2,13 @@ def display_menu
   puts "\nWelcome to our Library!"
   puts "\nPlease choose an option by entering a number:"
   options = [
-    '1 - List all books',
-    '2 - List all music albums',
-    '3 - List all games',
-    '4 - List all genres',
-    '5 - List all labels',
-    '6 - List all authors',
-    '7 - List all sources',
-    '8 - Add a book',
-    '9 - Add a music album',
-    '10 - Add a game',
-    '11 - Exit'
+    '1 - List all books, music albums, and games',
+    '2 - List all movies',
+    '3 - List all genres',
+    '4 - List all Labels',
+    '5 - List all Authors and Sources',
+    '6 - Add a book  or Add a music album or Add a game',
+    '7 - Exit'
   ]
   puts options
 end
