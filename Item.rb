59bf48fc58889d486a51361ff4254f070890a778
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-08-01 18:37:22 +0800

    All Item class properties visible in the diagram should be defined and set up in the constructor method

--- Item.rb
+++ Item.rb
@@ -1,3 +1,10 @@
 class Item
-  puts 'Hello World'
+  attr_writer :genre, :author, :source, :label
+
+  def initialize(publish_date, archived)
+    super()
+    @id = Random.rand(1..1000)
+    @publish_date = publish_date
+    @archived = archived
+  end
 end
